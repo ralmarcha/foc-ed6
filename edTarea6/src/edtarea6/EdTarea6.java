e007069e8914123695063d7318dca4d7a2220291
RaquelAlmarcha <ralmarcha@gmail.com>
2024-05-15 22:15:06 +0200

    second commit

---
+++ edTarea6/src/edtarea6/EdTarea6.java
@@ -0,0 +1,17 @@
+
+package edtarea6;
+
+public class EdTarea6 {
+
+   
+    public static void main(String[] args) {
+
+
+    Tarea6EDClase miCarrito = new Tarea6EDClase();
+
+    miCarrito.aplicarDescuento(100, 5);
+}
+
+    
+   
+}
